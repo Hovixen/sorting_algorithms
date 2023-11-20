ace676f437badaa778c9ee45527b0f8facff39ab
Hovixen <orevaerijo@gmail.com>
2023-11-20 15:08:51 +0000

    [Updated the code to use a swap function]

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,53 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * insertion_sort_list - sorts a DLL of integers in
- * ascending order using the insertion sort
- * algorithm
- *
- * @list: doubly linked list
- * Return: no return
+ * swap_nodes - function swaps the node of in doubly linked list
+ * @list: pointer to the pointer to list
+ * @n1: pointer to the first node
+ * @n2: pointer to the second node
+ * Return: returns void
  */
+
+void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2)
+{
+	if (n1->prev)
+		n1->prev->next = n2;
+	else
+		*list = n2;
+	if (n2->next)
+		n2->next->prev = n1;
+
+	n2->prev = n1->prev;
+	n1->next = n2->next;
+
+	n1->prev = n2;
+	n2->next = n1;
+}
+
+/**
+ * insertion_sort_list - function sorts list using insertion sort algorithm
+ * @list: pointer of list pointer
+ * Return: returns void
+ */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *ptr, *tmp;
+	listint_t *s_list;/* sorted part of list */
+	listint_t *u_list; /*unsorted part of list */
 
-	if (!list)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	ptr = *list;
-
-	while (ptr)
+	u_list = (*list)->next;
+	while (u_list && u_list->prev)
 	{
-		while (ptr->next && (ptr->n > ptr->next->n))
+		s_list = u_list->prev;
+		while (s_list && s_list->n > u_list->n)
 		{
-			tmp = ptr->next;
-			ptr->next = tmp->next;
-			tmp->prev = ptr->prev;
-
-			if (ptr->prev)
-				ptr->prev->next = tmp;
-
-			if (tmp->next)
-				tmp->next->prev = ptr;
-
-			ptr->prev = tmp;
-			tmp->next = ptr;
-
-			if (tmp->prev)
-				ptr = tmp->prev;
-			else
-				*list = tmp;
-
+			swap_nodes(list, s_list, u_list);
 			print_list(*list);
+			s_list = u_list->prev;
 		}
-		ptr = ptr->next;
+		u_list = u_list->next;
 	}
 }
