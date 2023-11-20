04fc3e67f79263b893612cbd3b516ea5fe87a5e3
Hovixen <orevaerijo@gmail.com>
2023-11-20 13:58:13 +0000

    [Updated the code to use a swap function]

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,28 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - function swaps the node of in doubly linked list
+ * @n1: pointer to the first node
+ * @n2: pointer to the second node
+ * Return: returns void
+ */
+
+void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2)
+{
+	if (n1->prev)
+		n1->prev->next = n2;
+	else
+		*list = n2;
+	if (n2->next)
+		n2->next->prev = n1;
+	n1->next = n2->next;
+	n2->prev = n1->prev;
+
+	n1->prev = n2;
+	n2->next = n1;
+}
+
+/**
  * insertion_sort_list - function sorts list using insertion sort algorithm
  * @list: pointer of list pointer
  * Return: returns void
@@ -8,37 +30,21 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *s_list = NULL; /*sorted part of list */
+	listint_t *s_list;/* sorted part of list */
 	listint_t *u_list; /*unsorted part of list */
-	listint_t *curr, *tmp;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL || *list == NULL)
 		return;
-	u_list = *list;
+	u_list = (*list)->next;
 	while (u_list)
 	{
-		curr = u_list;
+		s_list = u_list->prev;
+		while (s_list && s_list->n > u_list->n)
+		{
+			swap_nodes(list, s_list, u_list);
+			print_list(*list);
+			s_list = u_list->prev;
+		}
 		u_list = u_list->next;
-		if (!s_list || s_list->n >= curr->n)
-		{
-			curr->next = s_list;
-			curr->prev = NULL;
-			if (s_list)
-				s_list->prev = curr;
-			s_list = curr;
-		}
-		else
-		{
-			tmp = s_list;
-			while (tmp->next && tmp->next->n < curr->n)
-				tmp = tmp->next;
-			curr->next = tmp->next;
-			curr->prev = tmp;
-			if (tmp->next)
-				tmp->next->prev = curr;
-			tmp->next = curr;
-		}
-		print_list(s_list);
 	}
-	*list = s_list;
 }
--- sort.h
+++ sort.h
@@ -28,4 +28,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
 
+
+
+
+/*----helper swaps functions-----*/
+void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2);
+
 #endif
