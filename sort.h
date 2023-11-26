2703766882656a11d3a82262d28d14c3ea5c5227
Eric <stephen.erich243@gmail.com>
2023-11-25 17:47:05 -0800

    sorting

--- sort.h
+++ sort.h
@@ -32,6 +32,10 @@ void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(listint_t **list)
 
 /*----helper swaps functions-----*/
 void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2);
@@ -46,5 +50,14 @@ void merge(int *arr, int *temp, int start, int mid, int end);
 void *_calloc(unsigned int num, unsigned int size);
 void heapSwap(int *arr, int item1, int item2);
 void max_heapify(int *arr, size_t size, int idx, size_t n);
+int getMax(int *arr, int n);
+void countSort(int *arr, size_t n, int exp, int *output);
+void swap(int arr[], int item1, int item2, int order);
+void merge_bi(int arr[], int low, int nelemnt, int order);
+void bitonicsort(int arr[], int low, int nelemnt, int order, int size);
+void swap_qs(int *array, ssize_t item1, ssize_t item2);
+int hoare_partition(int *array, int first, int last, int size);
+void qs(int *array, ssize_t first, ssize_t last, int size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 
 #endif
